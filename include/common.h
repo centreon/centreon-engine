2537a06126c98ce1903c809255d47e90e286d912
Matthieu Kermagoret <mkermagoret@merethis.com>
2011-03-17 09:15:20 +0100

    Updated include/common.h header.

--- include/common.h
+++ include/common.h
@@ -1,25 +1,23 @@
-/************************************************************************
- *
- * Nagios Common Header File
- * Written By: Ethan Galstad (egalstad@nagios.org)
- * Last Modified: 10-22-2007
- *
- * License:
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- ************************************************************************/
-
+/*
+** Copyright 1999-2009 Ethan Galstad
+** Copyright 2009-2011 Nagios Core Development Team and Community Contributors
+** Copyright 2011      Merethis
+**
+** This file is part of Centreon Scheduler.
+**
+** Centreon Scheduler is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Scheduler is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Scheduler. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
 
 #define PROGRAM_VERSION "3.2.3"
 #define PROGRAM_MODIFICATION_DATE "10-03-2010"
