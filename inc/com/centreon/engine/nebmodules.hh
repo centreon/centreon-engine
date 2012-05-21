888d1eefba17e55477c4ff62a3a757d1ea6d3cf1
Matthieu Kermagoret <mkermagoret@merethis.com>
2012-05-21 09:58:34 +0200

    Engine now provide a NEB flag to notify modules of its extended features.

--- inc/com/centreon/engine/nebmodules.hh
+++ inc/com/centreon/engine/nebmodules.hh
@@ -23,11 +23,11 @@
 
 #  include <pthread.h>
 
-// Module version information.
+/* Module version information. */
 #  define NEB_API_VERSION(x) int __neb_api_version = x;
 #  define CURRENT_NEB_API_VERSION 3
 
-// Module information.
+/* Module information. */
 #  define NEBMODULE_MODINFO_NUMITEMS 6
 #  define NEBMODULE_MODINFO_TITLE 0
 #  define NEBMODULE_MODINFO_AUTHOR 1
@@ -36,12 +36,13 @@
 #  define NEBMODULE_MODINFO_LICENSE 4
 #  define NEBMODULE_MODINFO_DESC 5
 
-// Module load/unload options.
-#  define NEBMODULE_NORMAL_LOAD 0
+/* Module load/unload flags. */
+#  define NEBMODULE_NORMAL_LOAD    0
+#  define NEBMODULE_ENGINE         (1 << 12)
 #  define NEBMODULE_REQUEST_UNLOAD 0
-#  define NEBMODULE_FORCE_UNLOAD 1
+#  define NEBMODULE_FORCE_UNLOAD   (1 << 0)
 
-// Modules unload reasons.
+/* Modules unload reasons. */
 #  define NEBMODULE_NEB_SHUTDOWN 1
 #  define NEBMODULE_NEB_RESTART 2
 #  define NEBMODULE_ERROR_NO_INIT 3
@@ -50,13 +51,13 @@
 
 #  ifdef __cplusplus
 extern "C" {
-#  endif // C++
+#  endif /* C++ */
 
 int neb_set_module_info(void* handle, int type, char const* data);
 
 #  ifdef __cplusplus
 }
-#  endif // C++
+#  endif /* C++ */
 
 /**
  *  @struct nebmodule nebmodules.hh "com/centreon/engine/nebmodules.hh"
@@ -77,4 +78,4 @@ typedef struct             nebmodule_struct {
   struct nebmodule_struct* next;
 }                          nebmodule;
 
-#endif // !CCE_NEBMODULES_HH
+#endif /* !CCE_NEBMODULES_HH */
