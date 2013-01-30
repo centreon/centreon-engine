5deafe1b71e7e1f809cb57ee5845a258e491bd9e
Dorian Guillois <dguillois@merethis.com>
2013-01-30 10:42:33 +0100

    Add unit test on get next valid time.

---
+++ test/core/libtesttime.cc
@@ -0,0 +1,93 @@
+/*
+** Copyright 2013 Merethis
+**
+** This file is part of Centreon Engine.
+**
+** Centreon Engine is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Engine is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Engine. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <ctime>
+#include "com/centreon/engine/objects/timeperiod.hh"
+#include "com/centreon/engine/utils.hh"
+#include "test/core/libtesttime.hh"
+
+// overload of libc time function.
+// always return "Mon Jan 28 13:00:00 CET 2013"
+extern "C" time_t time(time_t *t) throw () {
+  if (t)
+    *t = 1359370800;
+  return (1359370800);
+}
+
+using namespace com::centreon::engine;
+
+/**
+ *  Check valid time.
+ *
+ *  @param[in] p     Time period to check.
+ *  @param[in] pref  Preferred time.
+ *  @param[in] ref   Reference time.
+ *
+ *  @return True on success, false otherwise.
+ */
+bool core::check_valid_time(timeperiod* p, time_t pref, time_t ref) {
+  time_t valid(0);
+  get_next_valid_time(pref, &valid, p);
+  return (valid == ref);
+}
+
+/**
+ *  Build week days with specific range.
+ *
+ *  @param[out] range      Tab to fill.
+ *  @param[in]  range_str  Define range.
+ */
+void core::build_week_days(
+       std::vector<std::string>& range,
+       std::string const& range_str) {
+  static char const* tab_days[] = {
+    "sunday ",
+    "monday ",
+    "tuesday ",
+    "wednesday ",
+    "thursday ",
+    "friday ",
+    "saturday ",
+    NULL
+  };
+
+  for (unsigned int i(0); tab_days[i]; ++i)
+    range.push_back(tab_days[i] + range_str);
+}
+
+/**
+ *  Build timeperiod and fill it with specific range and exlusion.
+ *
+ *  @param[in] name     The timeperiod name.
+ *  @param[in] range    Week days and exception.
+ *  @param[in] exclude  Timeperiod exclusion.
+ *
+ *  @return New timeperiod.
+ */
+timeperiod* core::build_timeperiod(
+              std::string const& name,
+              std::vector<std::string> const& range,
+              std::vector<std::string> const& exclude) {
+  objects::add_timeperiod(name, name, range, exclude);
+  timeperiod* p(find_timeperiod(name.c_str()));
+  if (p)
+    for (timeperiodexclusion* e(p->exclusions); e; e = e->next)
+      e->timeperiod_ptr = find_timeperiod(e->timeperiod_name);
+  return (p);
+}
---
+++ test/core/valid_time_week_day_exclude_into_current_time.cc
@@ -0,0 +1,79 @@
+/*
+** Copyright 2013 Merethis
+**
+** This file is part of Centreon Engine.
+**
+** Centreon Engine is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Engine is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Engine. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <exception>
+#include <iostream>
+#include "com/centreon/engine/error.hh"
+#include "com/centreon/engine/globals.hh"
+#include "com/centreon/engine/objects/timeperiod.hh"
+#include "test/core/libtesttime.hh"
+#include "test/unittest.hh"
+
+using namespace com::centreon::engine;
+
+/**
+ *  Check that the get_next_valid_time function works properly.
+ *
+ *  @return 0 on success.
+ */
+int main_test(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  // globale variables.
+  time_t now(time(NULL));
+  std::string name("testing_timeperiod");
+  std::vector<std::string> range;
+  std::vector<std::string> exclude;
+
+  // check with week days and exclude time period.
+  // range exclude into the current time.
+
+  init_object_skiplists();
+
+  std::string ename("exclude_timeperiod");
+  std::vector<std::string> erange;
+  core::build_week_days(erange, "11:00-13:00");
+  timeperiod* e(core::build_timeperiod(ename, erange, exclude));
+
+  exclude.push_back(ename);
+  core::build_week_days(range, "00:00-24:00");
+  timeperiod* p(core::build_timeperiod(name, range, exclude));
+  bool ret(core::check_valid_time(p, now, now));
+  objects::release(p);
+  objects::release(e);
+
+  if (!ret) {
+    free_memory(get_global_macros());
+    throw (engine_error() << "week days and exclude: "
+           "range exclude into the current time failed");
+  }
+
+  free_object_skiplists();
+
+  return (0);
+}
+
+/**
+ *  Init unit test.
+ */
+int main(int argc, char** argv) {
+  unittest utest(argc, argv, &main_test);
+  return (utest.run());
+}
