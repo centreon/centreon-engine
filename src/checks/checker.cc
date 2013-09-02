2a023acb03fb20a7808619f40cde60e1f0375127
Matthieu Kermagoret <mkermagoret@merethis.com>
2013-09-02 14:21:25 +0200

    Host and service check viability check should be handled as an error.
    Because the previous handling of the viability check was to silently
    fail, checks were not rescheduled in case of failure.
    
    This commit fixes #4852.

--- src/checks/checker.cc
+++ src/checks/checker.cc
@@ -231,7 +231,8 @@ void checker::run(
         check_options,
         time_is_valid,
         preferred_time) == ERROR)
-    return;
+    throw (engine_error() << "check of host '" << hst->name
+           << "' is not viable");
 
   // Don't execute a new host check if one is already running.
   if (hst->is_executing
@@ -239,7 +240,7 @@ void checker::run(
     logger(dbg_checks, basic)
       << "A check of this host (" << hst->name
       << ") is already being executed, so we'll pass for the moment...";
-    return;
+    return ;
   }
 
   // Send broker event.
@@ -464,7 +465,8 @@ void checker::run(
         check_options,
         time_is_valid,
         preferred_time) == ERROR)
-    return;
+    throw (engine_error() << "check of service '" << svc->description
+           << "' on host '" << svc->host_name << "' is not viable");
 
   // Send broker event.
   timeval start_time;
