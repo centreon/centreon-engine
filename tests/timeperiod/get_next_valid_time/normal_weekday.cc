34b8b621f5972a76303f9b27238b9213bd4bdf73
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-04 17:08:04 +0100

    Update normal weekday tests.

--- tests/timeperiod/get_next_valid_time/normal_weekday.cc
+++ tests/timeperiod/get_next_valid_time/normal_weekday.cc
@@ -29,51 +29,21 @@ using namespace com::centreon::engine;
 
 class         GetNextValidTimeNormalWeekdayTest : public ::testing::Test {
  public:
-  void        SetUp() {
-    _timeperiod = new timeperiod();
+  void        default_data_set() {
+    _creator.new_timeperiod();
     // tuesday 10:30-11:45,18:30-23:30
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      2,
-      hmtos(10, 30),
-      hmtos(11, 45));
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      2,
-      hmtos(18, 30),
-      hmtos(23, 30));
+    _creator.new_timerange(10, 30, 11, 45, 2);
+    _creator.new_timerange(18, 30, 23, 30, 2);
     // thursday 12:00-13:00
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      4,
-      hmtos(12, 0),
-      hmtos(13, 0));
+    _creator.new_timerange(12, 0, 13, 0, 4);
     // friday 08:30-09:15,12:15-12:45,18:30-19:45
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      5,
-      hmtos(8, 30),
-      hmtos(9, 15));
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      5,
-      hmtos(12, 15),
-      hmtos(12, 45));
-    add_timerange_to_timeperiod(
-      _timeperiod,
-      5,
-      hmtos(18, 30),
-      hmtos(19, 45));
-  }
-
-  void        TearDown() {
-    deleter::timeperiod(_timeperiod);
-    _timeperiod = NULL;
-    return ;
+    _creator.new_timerange(8, 30, 9, 15, 5);
+    _creator.new_timerange(12, 15, 12, 45, 5);
+    _creator.new_timerange(18, 30, 19, 45, 5);
   }
 
  protected:
-  timeperiod* _timeperiod;
+  timeperiod_creator _creator;
 };
 
 // Given a timeperiod configured with normal weekdays
@@ -81,11 +51,12 @@ class         GetNextValidTimeNormalWeekdayTest : public ::testing::Test {
 // When get_next_valid_time() is called
 // Then the next valid time is the beginning of the weekday's timerange
 TEST_F(GetNextValidTimeNormalWeekdayTest, BeforeWeekdays) {
+  default_data_set();
   time_t now(strtotimet("2016-10-24 12:00:00"));
   set_time(now);
-  time_t expected((time_t)-1);
-  get_next_valid_time(now, &expected, _timeperiod);
-  ASSERT_EQ(expected, strtotimet("2016-10-25 10:30:00"));
+  time_t computed((time_t)-1);
+  get_next_valid_time(now, &computed, _creator.get_timeperiods());
+  ASSERT_EQ(computed, strtotimet("2016-10-25 10:30:00"));
 }
 
 // Given a timeperiod configured with normal weekdays
@@ -93,11 +64,12 @@ TEST_F(GetNextValidTimeNormalWeekdayTest, BeforeWeekdays) {
 // When get_next_valid_time() is called
 // Then the next valid time is the beginning of the next weekday's timerange
 TEST_F(GetNextValidTimeNormalWeekdayTest, BetweenWeekdays) {
+  default_data_set();
   time_t now(strtotimet("2016-10-26 12:00:00"));
   set_time(now);
-  time_t expected((time_t)-1);
-  get_next_valid_time(now, &expected, _timeperiod);
-  ASSERT_EQ(expected, strtotimet("2016-10-27 12:00:00"));
+  time_t computed((time_t)-1);
+  get_next_valid_time(now, &computed, _creator.get_timeperiods());
+  ASSERT_EQ(computed, strtotimet("2016-10-27 12:00:00"));
 }
 
 // Given a timeperiod configured with normal weekdays
@@ -105,11 +77,12 @@ TEST_F(GetNextValidTimeNormalWeekdayTest, BetweenWeekdays) {
 // When get_next_valid_time() is called
 // Then the next valid time is now
 TEST_F(GetNextValidTimeNormalWeekdayTest, WithinWeekday) {
+  default_data_set();
   time_t now(strtotimet("2016-10-28 18:36:00"));
   set_time(now);
-  time_t expected((time_t)-1);
-  get_next_valid_time(now, &expected, _timeperiod);
-  ASSERT_EQ(expected, now);
+  time_t computed((time_t)-1);
+  get_next_valid_time(now, &computed, _creator.get_timeperiods());
+  ASSERT_EQ(computed, now);
 }
 
 // Given a timeperiod configured with normal weekdays
@@ -117,9 +90,10 @@ TEST_F(GetNextValidTimeNormalWeekdayTest, WithinWeekday) {
 // When get_next_valid_time() is called
 // Then the next valid time is the next week's first week day
 TEST_F(GetNextValidTimeNormalWeekdayTest, AfterWeekdays) {
+  default_data_set();
   time_t now(strtotimet("2016-10-29 13:37:42"));
   set_time(now);
-  time_t expected((time_t)-1);
-  get_next_valid_time(now, &expected, _timeperiod);
-  ASSERT_EQ(expected, strtotimet("2016-11-01 10:30:00"));
+  time_t computed((time_t)-1);
+  get_next_valid_time(now, &computed, _creator.get_timeperiods());
+  ASSERT_EQ(computed, strtotimet("2016-11-01 10:30:00"));
 }
