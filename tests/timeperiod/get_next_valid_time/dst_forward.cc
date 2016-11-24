ad747049476e97dc736f2e05b56472fe990785a1
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-24 11:41:27 +0100

    Reenable forward DST unit tests.

--- tests/timeperiod/get_next_valid_time/dst_forward.cc
+++ tests/timeperiod/get_next_valid_time/dst_forward.cc
@@ -74,20 +74,6 @@ class        GetNextValidTimeForwardDST : public ::testing::Test {
     _creator.new_timerange(3, 0, 4, 0, dr);
   }
 
-  void       timerange_within_dst(daterange* dr) {
-    _creator.new_timerange(2, 15, 2, 45, dr);
-    _creator.new_timerange(4, 0, 5, 0, dr);
-  }
-
-  void       timerange_stop_in_dst(daterange* dr) {
-    _creator.new_timerange(1, 0, 2, 30, dr);
-    _creator.new_timerange(4, 0, 5, 0, dr);
-  }
-
-  void       timerange_start_in_dst(daterange* dr) {
-    _creator.new_timerange(2, 30, 4, 0, dr);
-  }
-
   //
   // CURRENT TIME
   //
@@ -107,6 +93,9 @@ class        GetNextValidTimeForwardDST : public ::testing::Test {
 // RESULTS WILL BE MOSTLY THE SAME FOR ALL DATE RANGES
 //
 // So instead of writing all Given/When/Then, here are the common cases.
+// Please note that no test with timeranges starting in, stopping in or
+// within the DST forward gap exist. This is because no assumption can
+// be made on such ranges.
 //
 // Given a timeperiod covering a forward DST change
 // And the timeranges includes the DST range
@@ -119,24 +108,6 @@ class        GetNextValidTimeForwardDST : public ::testing::Test {
 // And we are at DST
 // When get_next_valid_time() is callled
 // Then the next valid time is the beginning of the timerange in the new timezone
-//
-// Given a timeperiod covering a forward DST change
-// And the timeranges are within the DST range
-// And we are at DST
-// When get_next_valid_time() is called
-// Then the next valid time is the beginning of the next timerange
-//
-// Given a timeperiod covering a forward DST change
-// And the timeranges stops in the skipped hour
-// And we are at DST
-// When get_next_valid_time() is called
-// Then the next valid time is the beginning of the next timerange
-//
-// Given a timeperiod covering a forward DST change
-// And the timeranges starts during the skipped hour
-// And we are at DST
-// When get_next_valid_time() is called
-// Then the next valid time is now
 
 
 //
@@ -159,25 +130,100 @@ TEST_F(GetNextValidTimeForwardDST, CalendarDateExcludeDSTNowAtDST) {
   ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
 }
 
-TEST_F(GetNextValidTimeForwardDST, CalendarDateWithinDSTNowAtDST) {
-  daterange* dr(dst_calendar_date());
-  timerange_within_dst(dr);
+//
+// SPECIFIC MONTH DATE TESTS
+//
+
+TEST_F(GetNextValidTimeForwardDST, SpecificMonthDateIncludeDSTNowAtDST) {
+  daterange* dr(dst_specific_month_date());
+  timerange_includes_dst(dr);
   at_dst();
   get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
-  ASSERT_EQ(_computed, strtotimet("2017-03-26 04:00:00"));
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
 }
 
-TEST_F(GetNextValidTimeForwardDST, CalendarDateStopInDSTNowAtDST) {
-  daterange* dr(dst_calendar_date());
-  timerange_stop_in_dst(dr);
+TEST_F(GetNextValidTimeForwardDST, SpecificMonthDateExcludeDSTNowAtDST) {
+  daterange* dr(dst_specific_month_date());
+  timerange_excludes_dst(dr);
   at_dst();
   get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
-  ASSERT_EQ(_computed, strtotimet("2017-03-26 04:00:00"));
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
 }
 
-TEST_F(GetNextValidTimeForwardDST, CalendarDateStartInDSTNowAtDST) {
-  daterange* dr(dst_calendar_date());
-  timerange_start_in_dst(dr);
+//
+// GENERIC MONTH DATE TESTS
+//
+
+TEST_F(GetNextValidTimeForwardDST, GenericMonthDateIncludeDSTNowAtDST) {
+  daterange* dr(dst_generic_month_date());
+  timerange_includes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+TEST_F(GetNextValidTimeForwardDST, GenericMonthDateExcludeDSTNowAtDST) {
+  daterange* dr(dst_generic_month_date());
+  timerange_excludes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+//
+// OFFSET WEEKDAY OF SPECIFIC MONTH DATE TESTS
+//
+
+TEST_F(GetNextValidTimeForwardDST, OffsetWeekdayOfSpecificMonthIncludeDSTNowAtDST) {
+  daterange* dr(dst_offset_weekday_of_specific_month());
+  timerange_includes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+TEST_F(GetNextValidTimeForwardDST, OffsetWeekdayOfSpecificMonthExcludeDSTNowAtDST) {
+  daterange* dr(dst_offset_weekday_of_specific_month());
+  timerange_excludes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+//
+// OFFSET WEEKDAY OF GENERIC MONTH DATE TESTS
+//
+
+TEST_F(GetNextValidTimeForwardDST, OffsetWeekdayOfGenericMonthIncludeDSTNowAtDST) {
+  daterange* dr(dst_offset_weekday_of_generic_month());
+  timerange_includes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+TEST_F(GetNextValidTimeForwardDST, OffsetWeekdayOfGenericMonthExcludeDSTNowAtDST) {
+  daterange* dr(dst_offset_weekday_of_generic_month());
+  timerange_excludes_dst(dr);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+//
+// NORMAL WEEKDAY TESTS
+//
+
+TEST_F(GetNextValidTimeForwardDST, NormalWeekdayIncludeDSTNowAtDST) {
+  _creator.new_timerange(1, 0, 4, 0, 0);
+  at_dst();
+  get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
+  ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
+}
+
+TEST_F(GetNextValidTimeForwardDST, NormalWeekdayExcludeDSTNowAtDST) {
+  _creator.new_timerange(1, 0, 2, 0, 0);
+  _creator.new_timerange(3, 0, 4, 0, 0);
   at_dst();
   get_next_valid_time(_now, &_computed, _creator.get_timeperiods());
   ASSERT_EQ(_computed, strtotimet("2017-03-26 03:00:00"));
