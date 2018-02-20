8db246ac3bcbe328bbbb4459ae4f327bb3f9d5fd
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-02-20 12:09:24 +0100

    chore(build): update to 1.8.2.

--- src/main.cc
+++ src/main.cc
@@ -198,7 +198,7 @@ int main(int argc, char* argv[]) {
         << "\n"
         << "Copyright 1999-2009 Ethan Galstad\n"
         << "Copyright 2009-2010 Nagios Core Development Team and Community Contributors\n"
-        << "Copyright 2011-2016 Centreon\n"
+        << "Copyright 2011-2018 Centreon\n"
         << "\n"
         << "This program is free software: you can redistribute it and/or\n"
         << "modify it under the terms of the GNU General Public License version 2\n"
