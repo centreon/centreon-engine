a826a0aac948061adc67e19a19f9956e114370d7
Luiz Gustavo S. Costa <luizgustavo@luizgustavo.pro.br>
2018-08-14 10:40:48 +0100

    doc: improve installation instructions
    * update content with new repository and CentOS7
    
    * remove information about cmake manually

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Centreon Engine'
-copyright = u'2013-2017 Centreon'
+copyright = u'2013-2018 Centreon'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
