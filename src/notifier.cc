91d2c3e03c903fa7b7a4b22a2ccf30fb0fb2fd83
David Boucher <dboucher@centreon.com>
2019-07-18 19:11:27 +0200

    fix(loop): Configuration reload in parallel improved.

--- src/notifier.cc
+++ src/notifier.cc
@@ -150,9 +150,8 @@ notifier::notifier(notifier::notifier_type notifier_type,
       _retain_nonstatus_information{retain_nonstatus_information},
       _is_being_freshened{false},
       _notification_number{0},
-      _pending_flex_downtime{0},
-      _state_history{}
-      {
+      _state_history{},
+      _pending_flex_downtime{0} {
   if (retry_interval <= 0) {
     logger(log_config_error, basic)
         << "Error: Invalid notification_interval value for notifier '"
