1a282e9e6b811571017d65fee187f3f68cdcdd41
David Boucher <dboucher@centreon.com>
2019-09-23 11:40:05 +0200

    fix(configuration): Configuration logs should be emitted with the config flag.

--- src/configuration/hostgroup.cc
+++ src/configuration/hostgroup.cc
@@ -93,42 +93,42 @@ hostgroup& hostgroup::operator=(hostgroup const& right) {
  */
 bool hostgroup::operator==(hostgroup const& right) const throw () {
   if (!object::operator==(right)) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => object don't match";
     return false;
   }
   if (_action_url != right._action_url) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => action url don't match";
     return false;
   }
   if (_alias != right._alias) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => alias don't match";
     return false;
   }
   if (_hostgroup_id != right._hostgroup_id) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => hostgroup id don't match";
     return false;
   }
   if (_hostgroup_name != right._hostgroup_name) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => hostgroup name don't match";
     return false;
   }
   if (_members != right._members) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => members don't match";
     return false;
   }
   if (_notes != right._notes) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => notes don't match";
     return false;
   }
   if (_notes_url != right._notes_url) {
-    logger(log_process_info, most)
+    logger(dbg_config, more)
       << "configuration::hostgroup::equality => notes url don't match";
     return false;
   }
