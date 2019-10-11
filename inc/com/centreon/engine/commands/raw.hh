fc857ad132308e34a8f6905f0df97c2d23013308
David Boucher <dboucher@centreon.com>
2019-10-11 18:38:23 +0200

    enh(raw): mutex updated and list changed into deque

--- inc/com/centreon/engine/commands/raw.hh
+++ inc/com/centreon/engine/commands/raw.hh
@@ -18,83 +18,69 @@
 */
 
 #ifndef CCE_COMMANDS_RAW_HH
-#  define CCE_COMMANDS_RAW_HH
+#define CCE_COMMANDS_RAW_HH
 
-#  include <list>
-#  include <string>
-#  include "com/centreon/concurrency/mutex.hh"
-#  include "com/centreon/engine/commands/command.hh"
-#  include "com/centreon/engine/namespace.hh"
-#  include "com/centreon/process.hh"
-#  include "com/centreon/process_listener.hh"
+#include <deque>
+#include <mutex>
+#include <string>
+#include "com/centreon/engine/commands/command.hh"
+#include "com/centreon/engine/namespace.hh"
+#include "com/centreon/process.hh"
+#include "com/centreon/process_listener.hh"
 
 CCE_BEGIN()
 
-namespace               commands {
-  class environment;
+namespace commands {
+class environment;
 
-  /**
-   *  @class raw raw.hh
-   *  @brief Raw is a specific implementation of command.
-   *
-   *  Raw is a specific implementation of command.
-   */
-  class                 raw
-    : public command,
-      public process_listener {
-  public:
-                        raw(
-                          std::string const& name,
-                          std::string const& command_line,
-                          command_listener* listener = NULL);
-                        raw(raw const& right);
-                        ~raw() throw () override;
-    raw&                operator=(raw const& right);
-    command*            clone() const override;
-    unsigned long       run(
-                          std::string const& process_cmd,
-                          nagios_macros& macros,
-                          unsigned int timeout) override;
-    void                run(
-                          std::string const& process_cmd,
-                          nagios_macros& macros,
-                          unsigned int timeout,
-                          result& res) override;
+/**
+ *  @class raw raw.hh
+ *  @brief Raw is a specific implementation of command.
+ *
+ *  Raw is a specific implementation of command.
+ */
+class raw : public command, public process_listener {
+  std::mutex _lock;
+  std::unordered_map<process*, uint64_t> _processes_busy;
+  std::deque<process*> _processes_free;
 
-  private:
-    void                data_is_available(process& p) throw () override;
-    void                data_is_available_err(process& p) throw () override;
-    void                finished(process& p) throw () override;
-    static void         _build_argv_macro_environment(
-                          nagios_macros const& macros,
-                          environment& env);
-    static void         _build_contact_address_environment(
-                          nagios_macros const& macros,
-                          environment& env);
-    static void         _build_custom_contact_macro_environment(
-                          nagios_macros& macros,
-                          environment& env);
-    static void         _build_custom_host_macro_environment(
-                          nagios_macros& macros,
-                          environment& env);
-    static void         _build_custom_service_macro_environment(
-                          nagios_macros& macros,
-                          environment& env);
-    static void         _build_environment_macros(
-                          nagios_macros& macros,
-                          environment& env);
-    static void         _build_macrosx_environment(
-                          nagios_macros& macros,
-                          environment& env);
-    process*            _get_free_process();
+  void data_is_available(process& p) noexcept override;
+  void data_is_available_err(process& p) noexcept override;
+  void finished(process& p) noexcept override;
+  static void _build_argv_macro_environment(nagios_macros const& macros,
+                                            environment& env);
+  static void _build_contact_address_environment(nagios_macros const& macros,
+                                                 environment& env);
+  static void _build_custom_contact_macro_environment(nagios_macros& macros,
+                                                      environment& env);
+  static void _build_custom_host_macro_environment(nagios_macros& macros,
+                                                   environment& env);
+  static void _build_custom_service_macro_environment(nagios_macros& macros,
+                                                      environment& env);
+  static void _build_environment_macros(nagios_macros& macros,
+                                        environment& env);
+  static void _build_macrosx_environment(nagios_macros& macros,
+                                         environment& env);
+  process* _get_free_process();
 
-    concurrency::mutex  _lock;
-    std::unordered_map<process*, unsigned long>
-                        _processes_busy;
-    std::list<process*> _processes_free;
-  };
+ public:
+  raw(std::string const& name,
+      std::string const& command_line,
+      command_listener* listener = NULL);
+  raw(raw const& right);
+  ~raw() noexcept override;
+  raw& operator=(raw const& right);
+  command* clone() const override;
+  uint64_t run(std::string const& process_cmd,
+                    nagios_macros& macros,
+                    uint32_t timeout) override;
+  void run(std::string const& process_cmd,
+           nagios_macros& macros,
+           uint32_t timeout,
+           result& res) override;
+};
 }
 
 CCE_END()
 
-#endif // !CCE_COMMANDS_RAW_HH
+#endif  // !CCE_COMMANDS_RAW_HH
