e7337d1c2c383e902e658a622948b43f2437a7f4
David Boucher <dboucher@centreon.com>
2020-03-10 14:23:20 +0100

    fix(connector): timestamp is in milliseconds now.

--- src/commands/connector.cc
+++ src/commands/connector.cc
@@ -624,11 +624,11 @@ void connector::_recv_query_execute(char const* data) {
     res.exit_status = process::normal;
     res.start_time = info->start_time;
 
-    time_t execution_time((res.end_time - res.start_time).to_seconds());
+    time_t execution_time((res.end_time - res.start_time).to_mseconds());
 
     // Check if the check timeout.
     if (info->timeout > 0
-        && static_cast<unsigned int>(execution_time) > info->timeout) {
+        && static_cast<unsigned int>(execution_time) > info->timeout * 1000) {
       res.exit_status = process::timeout;
       res.output = "(Process Timeout)";
     }
