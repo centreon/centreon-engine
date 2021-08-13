a9a74e28b48052e3c8f932f34c8ea9b38f4df6da
centreonpm <pmargale@centreon.com>
2021-08-13 05:54:24 +0200

    enh(engine-rpc-client): init proto script works for broker

--- enginerpc/examples/init-proto.sh
+++ enginerpc/examples/init-proto.sh
@@ -1,1 +1,3 @@
-python3 -m grpc_tools.protoc -I.. --python_out=. --grpc_python_out=. ../engine.proto
+python3 -m grpc_tools.protoc -I.. --python_out=. --grpc_python_out=. ../engine.proto 
+python3 -m grpc_tools.protoc --proto_path=/root/centreon-broker/core/src/ --python_out=. --grpc_python_out=. broker.proto
+
